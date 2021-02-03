45441a54b4b8900d501be3b1e0e7fde83038b12f
camilosancro <kasanro@gmail.com>
2021-02-03 17:27:16 -0300

    se agregan los proceso a la peticion y se apunta a url de aws

--- monitor.py
+++ monitor.py
@@ -6,15 +6,17 @@ import random, json, subprocess, psutil, requests, platform, socket
 payload={}
 
 def servidor():
+    payload['servidor'] = []
     uname = platform.uname()
-    payload['nombre']= uname.node
-    payload['procesador']= uname.machine
-    payload['sistema_operativo']= uname.system
-    payload['version']= uname.version
-
-def cpu():
     cpu_usage = psutil.cpu_percent(interval=2)
-    payload['cpu'] = float(cpu_usage)
+ 
+    payload['servidor'].append({
+                    "nombre_servidor": uname.node,
+                    "procesador": uname.machine,
+                    "sistema_operativo": uname.system,
+                    "version": uname.version,
+                    "cpu_uso": float(cpu_usage)
+            })
 
 def ip_servidor():
     nombre_equipo = socket.gethostname()
@@ -25,8 +27,8 @@ def usuarios():
         payload['usuarios'] = []
         for usuario in psutil.users():
             payload['usuarios'].append({
-                    "nombre": usuario[0],
-                    "pid": usuario[4],
+                    "nombre_usuario": usuario[0],
+                    "pid_usuario": usuario[4],
                     "terminal": usuario[1]
             })
 
@@ -34,8 +36,8 @@ def procesos():
         payload['procesos'] = []
         for proc in psutil.process_iter(['pid', 'name', 'username']):
             payload['procesos'].append({
-                "nombre": proc.info['name'],
-                "pid" : proc.info['pid'],
+                "nombre_proceso": proc.info['name'],
+                "pid_proceso" : proc.info['pid'],
                 "username": proc.info['username']
             })    
 
@@ -45,8 +47,8 @@ def print_json():
 
 def post_info():
     #URL DE  SERVICIO REST
-    #url = 'http://54.236.147.177/registrar'
-    url = 'http://127.0.0.1:8080/registrar'
+    url = 'http://54.236.147.177/registrar'
+    #url = 'http://127.0.0.1:8080/registrar'
 
     response = requests.post(url, json=payload)
     print(response)
@@ -56,9 +58,8 @@ def post_info():
 
 if __name__ == "__main__":
     servidor()
-    cpu()
     ip_servidor()
     usuarios()
     procesos()
     print_json()
-    #post_info()
+    post_info()
